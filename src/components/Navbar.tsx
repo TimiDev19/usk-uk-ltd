4d060c7e0657b8ad88e5bbf74522c1d430d7f7fb
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-08-21 00:42:45 -0700

    navbar is now active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,36 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <div>
-            <div className="md:hidden fixed w-full flex items-center justify-between px-4 py-3 bg-white">
-                <h1 className=" uppercase text-lg">USV UK Limited</h1>
-                <div className=" text-black hover:cursor-pointer " onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    {
-                        isMenuOpen ? (<div><CloseOutlinedIcon /></div>) : (<div><MenuOutlinedIcon /></div>)
-                    }
+        <div className=' z-50 fixed w-full'>
+            <div className="md:hidden fixed w-full  px-4 py-3 bg-white">
+                <div className=" flex items-center justify-between mb-6">
+                    <h1 className=" uppercase text-lg">USV UK Limited</h1>
+                    <div className=" text-black hover:cursor-pointer " onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        {
+                            isMenuOpen ? (<div><CloseOutlinedIcon /></div>) : (<div><MenuOutlinedIcon /></div>)
+                        }
+                    </div>
                 </div>
+                {
+                    isMenuOpen && <div className=" w-full bg-white">
+                        <ul>
+                            <li>
+                                <Link to={''} className="block mb-4  uppercase hover:text-blue-500 duration-500">Home</Link>
+                            </li>
+                            <li>
+                                <Link to={''} className="block mb-4  uppercase hover:text-blue-500 duration-500">Shop</Link>
+                            </li>
+                            <li>
+                                <Link to={''} className="block  mb-4 uppercase hover:text-blue-500 duration-500">About</Link>
+                            </li>
+                            <li>
+                                <Link to={''} className="block mb-4  uppercase hover:text-blue-500 duration-500">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                }
             </div>
+
             <div className="md:flex fixed w-full hidden items-center justify-between px-4 py-3 bg-white">
                 <div>
                     <Link to={''} className=" mx-4 uppercase hover:text-blue-500 duration-500">Shop</Link>
@@ -31,6 +52,10 @@ const Navbar = () => {
                     <Link to={''} className=" mx-4 uppercase hover:text-blue-500 duration-500">Contact</Link>
                 </div>
             </div>
+
+
+
+
         </div>
     )
 }
